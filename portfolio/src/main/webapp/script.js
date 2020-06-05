9d7c1b8c40e3b3c89ad4e6150dcdd157ec1b6925
Kaye Fung <32723741+kayefung@users.noreply.github.com>
2020-06-05 10:10:22 -0400

    Remove comments in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -52,9 +52,6 @@ function addRandomFact() {
  * Fetches messages from server and adds it to DOM
  */
 function getComments() {
-  // current problem -> doesn't load any comments when you initially open pg
-  // because next line is null -> doesn't have a value
-  // selected="selected" doesn't work??
   var index = document.getElementById('max-comments').selectedIndex;
   var maxComments = document.getElementsByTagName('option')[index].value;
 
