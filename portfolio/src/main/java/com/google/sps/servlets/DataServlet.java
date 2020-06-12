4a25c4f5ec5bf1c12cb2c5a1d39b56dd2b6e0d8a
Kaye Fung <kayefung@outlook.com>
2020-06-12 10:55:33 -0400

    Change calculateSentimentScore from public to private method.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -100,7 +100,7 @@ public class DataServlet extends HttpServlet {
   /**
    * Calculates sentiment score of a comment's text and returns the score as a float. 
    */
-  public float calculateSentimentScore(String text) throws IOException {
+  private float calculateSentimentScore(String text) throws IOException {
     Document doc =
         Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
     LanguageServiceClient languageService = LanguageServiceClient.create();
