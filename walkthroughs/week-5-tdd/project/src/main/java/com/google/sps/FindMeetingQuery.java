d4aabd859e60d41b972f7cf009d592e2c0c4335a
Kaye Fung <kayefung@outlook.com>
2020-06-17 12:05:39 -0400

    Implement query to find times to host a meeting.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,57 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Collections;
+import com.google.sps.Event;
+import com.google.sps.MeetingRequest;
+import com.google.sps.TimeRange;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Do not provide any time options if the meeting requested is longer than a whole day. 
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    ArrayList<TimeRange> times = new ArrayList<>(Arrays.asList(TimeRange.WHOLE_DAY));
+
+    for (Event event : events) {
+
+      // Do not use event to determine time range options if none of the event attendees will be
+      // attending the requested meeting. 
+      if (Collections.disjoint(event.getAttendees(), request.getAttendees())) {
+        continue;
+      }
+
+      ListIterator<TimeRange> timesIterator = times.listIterator();
+
+      while (timesIterator.hasNext()) {
+        TimeRange time = timesIterator.next();
+
+        if (time.contains(event.getWhen()) || time.overlaps(event.getWhen())) {
+          // Remove time because event runs through it.
+          timesIterator.remove();
+          
+          // Split time into two time ranges, one before the event and after the event.
+          TimeRange beforeEvent = 
+              TimeRange.fromStartEnd(time.start(), event.getWhen().start(), false);
+          TimeRange afterEvent =
+              TimeRange.fromStartEnd(event.getWhen().end(), time.end(), false);
+          
+          // Only add new ranges if the meeting requested can occur in that time range.
+          if (beforeEvent.duration() >= request.getDuration()) {
+            timesIterator.add(beforeEvent);
+          }
+          if (afterEvent.duration() >= request.getDuration()) {
+            timesIterator.add(afterEvent);
+          }
+        }
+      }
+    }
+
+    return times;
   }
 }
